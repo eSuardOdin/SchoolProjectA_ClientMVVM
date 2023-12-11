bb1a753ee42cd6e5bc48fc2c81c4e2202c7c6baf
eSuardOdin <esuardodin@gmail.com>
2023-12-11 06:19:09 +0100

    Basic handling ok, need to add all subViews/subViewModels

--- SchoolProjectA_ClientMVVM/ViewModels/GlobalViewModel.cs
+++ SchoolProjectA_ClientMVVM/ViewModels/GlobalViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +8,33 @@ using System.Threading.Tasks;
 namespace SchoolProjectA_ClientMVVM.ViewModels;
 public class GlobalViewModel : ViewModelBase
 {
+    private ViewModelBase _containedViewModel;
+    public GlobalViewModel()
+    {
+        _containedViewModel = new AccountsViewModel();
+    }
+
+    public ViewModelBase ContainedViewModel
+    {
+        get => _containedViewModel;
+        set => this.RaiseAndSetIfChanged(ref _containedViewModel, value);
+    }
+
+    public void ShowTransactions()
+    {
+        System.Diagnostics.Debug.WriteLine("Clic sur transaction");
+        // ContainedViewModel = new TransactionsViewModel();
+    }
+
+    public void ShowAccounts()
+    {
+        System.Diagnostics.Debug.WriteLine("Clic sur account");
+        // ContainedViewModel = new AccountsViewModel();
+    }
+
+    public void ShowTags()
+    {
+        System.Diagnostics.Debug.WriteLine("Clic sur tag");
+        // ContainedViewModel = new TagsViewModel();
+    }
 }
