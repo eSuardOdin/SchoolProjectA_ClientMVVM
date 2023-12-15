88d83b617fb4d077ca1445503a5b7d73efca22ba
eSuardOdin <esuardodin@gmail.com>
2023-12-15 06:39:59 +0100

    Added AddAccount basic View

--- SchoolProjectA_ClientMVVM/ViewModels/GlobalViewModels/SubViewModels/AddAccountViewModel.cs
+++ SchoolProjectA_ClientMVVM/ViewModels/GlobalViewModels/SubViewModels/AddAccountViewModel.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ReactiveUI;
+using SchoolProjectA_ClientMVVM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,4 +11,23 @@ namespace SchoolProjectA_ClientMVVM.ViewModels;
 
 public class AddAccountViewModel : ViewModelBase
 {
+    public string _bankAccountLabel = string.Empty;
+    public decimal _bankAccountBalance = 0;
+
+    public ReactiveCommand<Unit, BankAccount> AddCommand { get; }
+    public ReactiveCommand<Unit, Unit> CancelCommand { get; }
+
+
+    
+    public string BankAccountLabel
+    {
+        get => _bankAccountLabel;
+        set => this.RaiseAndSetIfChanged(ref _bankAccountLabel, value);
+    }
+
+    public decimal BankAccountBalance
+    {
+        get => _bankAccountBalance;
+        set => this.RaiseAndSetIfChanged(ref this._bankAccountBalance, value);
+    }
 }
