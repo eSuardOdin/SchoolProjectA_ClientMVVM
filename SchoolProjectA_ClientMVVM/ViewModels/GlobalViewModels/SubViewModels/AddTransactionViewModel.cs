e480b6e2c984a6024ddac5a63333e3032fef1761
eSuardOdin <esuardodin@gmail.com>
2023-12-26 05:55:03 +0100

    Advanced on View and VM

--- SchoolProjectA_ClientMVVM/ViewModels/GlobalViewModels/SubViewModels/AddTransactionViewModel.cs
+++ SchoolProjectA_ClientMVVM/ViewModels/GlobalViewModels/SubViewModels/AddTransactionViewModel.cs
@@ -7,6 +7,7 @@ using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
 using SchoolProjectA_ClientMVVM.Models;
+using System.Collections.ObjectModel;
 
 namespace SchoolProjectA_ClientMVVM.ViewModels;
 
@@ -16,9 +17,59 @@ public class AddTransactionViewModel : ViewModelBase
     private string? _transactionDescription;
     private decimal _transactionAmount;
     private DateTime _transactionDate;
-    private int _bankAccountId;
+    //private int _bankAccountId;
     private int[] _tags;
-    private List<BankAccount>? _bankAccounts;
+    private ObservableCollection<BankAccount>? _bankAccounts;
+
+    public string TransactionLabel
+    {
+        get => _transactionLabel;
+        set => this.RaiseAndSetIfChanged(ref _transactionLabel, value);
+    }
+
+    public string TransactionDescription
+    {
+        get => _transactionDescription;
+        set => this.RaiseAndSetIfChanged(ref _transactionDescription, value);
+    }
+
+    public decimal TransactionAmount
+    {
+        get => _transactionAmount;
+        set => this.RaiseAndSetIfChanged(ref _transactionAmount, value);
+    }
+
+    public DateTime TransactionDate
+    {
+        get => _transactionDate; 
+        set => this.RaiseAndSetIfChanged(ref _transactionDate, value);
+    }
+
+    public int[] Tags
+    {
+        get => _tags;
+        set => this.RaiseAndSetIfChanged(ref _tags, value);
+    }
+
+    public ObservableCollection<BankAccount> BankAccounts
+    {
+        get => _bankAccounts; 
+        set => this.RaiseAndSetIfChanged(ref _bankAccounts, value);
+    }
+
+    // Load accounts
+    private async Task<List<BankAccount>> LoadBankAccounts(int moniId)
+    {
+        List<BankAccount> bankAccounts = await Queries.GetMoniAccounts(moniId);
+        return bankAccounts;
+    }
+    // Casting list to ObservableCollection
+    private async void InitializeAsync(int moniId)
+    {
+        List<BankAccount> accounts = await LoadBankAccounts(moniId);
+        BankAccounts = new ObservableCollection<BankAccount>(accounts);
+    }
+
     public int MoniId {  get; set; }
 
     public ReactiveCommand<Unit, Transaction> AddTransactionCommand { get; set; }
@@ -27,6 +78,7 @@ public class AddTransactionViewModel : ViewModelBase
     public AddTransactionViewModel(int moniId)
     {
         MoniId = moniId;
+        InitializeAsync(MoniId);
     }
 
 
